f9dc1ce56ed3cb568a47fc3b5e95fdb9080d12aa
Philip Nilsson <Philip Nilsson>
2016-10-20 17:00:39 +0100

    Added test images to show example graphs

---
+++ Randomly Generate Graphs/graphs.cpp
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <iostream>
+#include <fstream>
+
+const int NUM_NODES = 6;
+const int MAX_FLOW = 1;
+
+void generateConnections();
+void randomlyPlaceNodes();
+
+int main (int argc, char[] argv*)
+{
+	
+}
+
+void generateConnections()
+{
+	std::vector<std::vector<int>> connections;
+}
