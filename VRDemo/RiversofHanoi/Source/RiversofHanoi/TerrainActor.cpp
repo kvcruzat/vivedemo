612e62a37b1cec9a6fb9b464b53aae4a7e4de1cf
Philip Nilsson <Philip Nilsson>
2016-11-01 13:49:29 +0000

    Fixed graph generation

--- VRDemo/RiversofHanoi/Source/RiversofHanoi/TerrainActor.cpp
+++ VRDemo/RiversofHanoi/Source/RiversofHanoi/TerrainActor.cpp
@@ -11,7 +11,7 @@ ATerrainActor::ATerrainActor()
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	readData("terrain2.m");
+	readData("terrain.m");
 
 	USphereComponent* SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("RootComponent"));
 	RootComponent = SphereComponent;
