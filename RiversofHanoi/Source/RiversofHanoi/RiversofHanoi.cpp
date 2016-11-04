b7bfcb2577d669a33157fb7f1b75ed686106866e
Khen Cruzat <sc13kvc@leeds.ac.uk>
2016-11-04 13:54:17 +0000

    Incorporated VR features

---
+++ RiversofHanoi/Source/RiversofHanoi/RiversofHanoi.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "RiversofHanoi.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, RiversofHanoi, "RiversofHanoi" );
--- VRDemo/RiversofHanoi/Source/RiversofHanoi/RiversofHanoi.h
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
-
-#ifndef __RIVERSOFHANOI_H__
-#define __RIVERSOFHANOI_H__
-
-#include "EngineMinimal.h"
-
-
-#endif
